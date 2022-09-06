cb71dc490f87c7a1fa57029e7c8c721811c3ff89
MehediHasanEmon21 <mh.emon2021@gmail.com>
2022-09-06 08:43:31 +0600

    add draggable persistance

--- js/app.js
+++ js/app.js
@@ -1,45 +1,59 @@
 
 const url = 'http://127.0.0.1:8000/api/v1/task';
 const task_status = document.querySelectorAll('.task-status');
-let draggableTodo = null;
 const todo_task_wrapper = document.querySelector('.todo-task');
-const textnode = document.createTextNode("Water");
+const progress_task_wrapper = document.querySelector('.progress-task');
+const done_wrapper = document.querySelector('.done-task');
+let draggableTodo = null;
+
+// fetch all task
 async function fetchTasks() {
-    let res = await fetch(`${url}`);
-    let { data } = await res.json();
-    displayTodoTask(data);
+    try {
+        let res = await fetch(`${url}`);
+        let { data } = await res.json();
+        const todo_tasks = data.filter(task => task.status == 0);
+        const progress_tasks = data.filter(task => task.status == 1);
+        const done_tasks = data.filter(task => task.status == 2);
+        displayTask(todo_tasks, todo_task_wrapper);
+        displayTask(progress_tasks, progress_task_wrapper);
+        displayTask(done_tasks, done_wrapper);
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
-function displayTodoTask(tasks){
-    todo_task_wrapper.innerHTML = `${tasks.map((task) => {
-        return `<h4 class="todo" draggable="true">${task.name}</h4>`
+function displayTask(tasks, wrapper) {
+    wrapper.innerHTML = `${tasks.map((task) => {
+        return `<h4 class="todo" draggable="true" data-id="${task.id}">${task.name}</h4>`
     }).join("")}`
     const todos = document.querySelectorAll(".todo");
     todos.forEach((todo) => {
-        todo.addEventListener('dragstart',dragStart);
-        todo.addEventListener('dragend',dragEnd);
+        todo.addEventListener('dragstart', dragStart);
+        todo.addEventListener('dragend', dragEnd);
     })
-    
+
 }
 fetchTasks();
 
-async function addTask(){
+// add task by api
+async function addTask() {
     const input_value = document.getElementById('input-form').value;
-    if(input_value){
-        const form_data = {name : input_value};
+    if (input_value) {
+        const form_data = { name: input_value };
         try {
             let res = await fetch(`${url}/create`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify( form_data ),
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                },
+                body: JSON.stringify(form_data),
             })
             let { data } = await res.json();
             const h4 = document.createElement("h4");
             h4.classList.add('todo');
-            h4.setAttribute('draggable',true);
+            h4.setAttribute('draggable', true);
             const text_node = document.createTextNode(`${data.name}`);
             h4.appendChild(text_node);
             todo_task_wrapper.appendChild(h4);
@@ -47,27 +61,25 @@ async function addTask(){
         } catch (error) {
             console.log(error);
         }
-        
-    }else{
+
+    } else {
         alert('Please Enter Task Name');
     }
 }
 
-
-
-
-function dragStart(){
+function dragStart() {
     draggableTodo = this;
     setTimeout(() => {
         this.style.display = "none";
-      }, 0);
+    }, 0);
 
 }
-function dragEnd(){
+
+function dragEnd() {
     draggableTodo = null;
     setTimeout(() => {
         this.style.display = "block";
-      }, 0);
+    }, 0);
 
 }
 
@@ -76,20 +88,51 @@ task_status.forEach((status) => {
     status.addEventListener("dragenter", dragEnter);
     status.addEventListener("dragleave", dragLeave);
     status.addEventListener("drop", dragDrop);
-  });
+});
 
-function dragOver(e){
+function dragOver(e) {
     e.preventDefault();
 }
 
-function dragEnter(){
-    
+function dragEnter() {
+
 }
 
-function dragLeave(){
-    
+function dragLeave() {
+
 }
 
-function dragDrop(){
+// drop the task
+async function dragDrop() {
+
+    let status;
+    const name = draggableTodo.innerText;
+    const id = draggableTodo.getAttribute('data-id');
     this.querySelector('.task-list').appendChild(draggableTodo);
+    const task_container = this.querySelector('.task-list');
+    if (task_container.classList.contains('todo-task')) {
+        status = 0;
+    } else if (task_container.classList.contains('progress-task')) {
+        status = 1;
+    } else {
+        status = 2;
+    }
+    const form_data = {
+        id,
+        status
+    }
+    try {
+        let res = await fetch(`${url}/update`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify(form_data),
+        })
+        let { data } = await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+
 }
